58ed7ffb8cf5e04d0d735946be770ec7e793e462
Miftachul Huda <miftachulcc@gmail.com>
2023-10-22 22:02:07 +0700

    refine directory

---
+++ src/preload/shift_tab.js
@@ -0,0 +1,30 @@
+const shift = require("./shift.js");
+const next = Date.now() + 3600000 * 8;
+const double_next = Date.now() + 3600000 * 16;
+
+function updateShift() {
+  document.getElementById("shiftblock1").innerHTML = shift
+    .checkOnDateShift(Date.now())
+    .split(" ")[0];
+  document.getElementById("shiftblock2").innerHTML = `&#x2794; ${
+    shift.checkOnDateShift(next).split(" ")[0]
+  }`;
+  document.getElementById("shiftblock3").innerHTML = `&#x2794; ${
+    shift.checkOnDateShift(double_next).split(" ")[0]
+  }`;
+  document.getElementById("masuk_apa1").innerHTML = `Shift ${
+    shift.checkOnDateShift(Date.now()).split(" ")[1]
+  }`;
+  document.getElementById("masuk_apa2").innerHTML = `Shift ${
+    shift.checkOnDateShift(next).split(" ")[1]
+  }`;
+  document.getElementById("masuk_apa3").innerHTML = `Shift ${
+    shift.checkOnDateShift(double_next).split(" ")[1]
+  }`;
+  //  document.getElementById('nextshift').innerHTML = `Next : ${listshift[daynext][getPeriod(minutesnext)]}`
+  console.log(shift.checkOnDateShift(Date.now()).split(" ")[0]);
+  console.log(shift.checkOnDateShift(next).split(" ")[0]);
+  console.log(shift.checkOnDateShift(double_next).split(" ")[0]);
+}
+
+module.exports = updateShift;
---
+++ src/renderer/titlebar.js
@@ -0,0 +1,17 @@
+const { RGBA } = require("custom-electron-titlebar");
+const customTitlebar = require("custom-electron-titlebar");
+const { remote } = require("electron");
+const url = require("url");
+const path = require("path");
+var pjson = require("../../package.json");
+
+let MyTitleBar = new customTitlebar.Titlebar({
+  backgroundColor: new customTitlebar.Color(new RGBA(25, 27, 37, 0.679)),
+  shadow: true,
+  titleHorizontalAlignment: "left",
+  icon: url.format(path.join(__dirname, "/img", "/ico.png")),
+});
+const menu = new remote.Menu();
+menu.append(new remote.MenuItem({}));
+MyTitleBar.updateTitle(`Tank Level v${pjson.version}`);
+MyTitleBar.updateMenu(null);
