284c3137f265e1e2f6c9df35a6b911050ebcc9c1
Miftachul Huda <miftachulcc@gmail.com>
2023-11-03 14:46:50 +0700

    add wine

--- src/compile.js
+++ src/compile.js
@@ -8,7 +8,7 @@ const mainPath = path.join(
 function execShellCommand(cmd) {
   const exec = require("child_process").exec;
   return new Promise((resolve, reject) => {
-    const child = exec(cmd, (error, stdout, stderr) => {
+    const child = exec("wine " + cmd, (error, stdout, stderr) => {
       if (error) {
         console.warn(error);
       }
